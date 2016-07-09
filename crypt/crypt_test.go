cab3c6307b0af2df52059a9c9f517c6c29ec7239
Jonathan <jonathan@jungledisk.com>
2016-07-09 12:01:08 -0500

    Update formatting slightly

--- crypt/crypt_test.go
+++ crypt/crypt_test.go
@@ -3,19 +3,18 @@ package crypt
 import "testing"
 
 func TestEncryptionAndDecryption(t *testing.T) {
+
 	if err := InitializeBlock("example key 1234"); err != nil {
 		t.Fatal(err)
 	}
 
 	unencryptedString := "exampleplaintext"
-
 	t.Logf("Unencrypted string: %s", unencryptedString)
 
 	encryptedHexString, err := EncryptStringToHexString(unencryptedString)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	t.Logf("Encrypted hex string: %s", encryptedHexString)
 
 	// ciphertext, _ := hex.DecodeString("f363f3ccdcb12bb883abf484ba77d9cd7d32b5baecb3d4b1b3e0e4beffdb3ded")
@@ -23,7 +22,6 @@ func TestEncryptionAndDecryption(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	t.Logf("Decrypted string: %s", decryptedString)
 
 	if unencryptedString != decryptedString {
