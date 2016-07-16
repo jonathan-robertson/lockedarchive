fd74491e99bc59b2e68fdcec5cbc06937fea4692
Jonathan <jonathan@jungledisk.com>
2016-07-16 14:21:00 -0500

    Update tests with varying inputs and expected outputs

--- crypt/crypt_test.go
+++ crypt/crypt_test.go
@@ -1,30 +1,81 @@
 package crypt
 
-import "testing"
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
 
-func TestEncryptionAndDecryption(t *testing.T) {
+func init() {
+	InitializeBlock("example key 1234")
+}
 
-	if err := InitializeBlock("example key 1234"); err != nil {
-		t.Fatal(err)
+func TestDecrypt(t *testing.T) {
+	encryptedInput, err := hex.DecodeString("175ca7c365899c8f03f75e21dce7e2046ad858ea9efee61e1e62325c87cb42ec9440f2e074cc5874a859a7af4033185f")
+	if err != nil {
+		panic(err)
 	}
+	expectedOutput := []byte("beepbeepbeepboooooooo-splosion")
 
-	unencryptedString := "exampleplaintext"
-	t.Logf("Unencrypted string: %s", unencryptedString)
-
-	encryptedHexString, err := EncryptStringToHexString(unencryptedString)
+	output, err := Decrypt(encryptedInput)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("Encrypted hex string: %s", encryptedHexString)
 
-	// ciphertext, _ := hex.DecodeString("f363f3ccdcb12bb883abf484ba77d9cd7d32b5baecb3d4b1b3e0e4beffdb3ded")
-	decryptedString, err := DecryptHexStringToString(encryptedHexString)
+	if bytes.Compare(output, expectedOutput) != 0 {
+		t.Fatalf("output does not match expected output\nout:\t%s\nexp:\t%s\n", output, expectedOutput)
+	}
+}
+
+func TestDecryptHexStringToString(t *testing.T) {
+	input := "f363f3ccdcb12bb883abf484ba77d9cd7d32b5baecb3d4b1b3e0e4beffdb3ded"
+	expectedOutput := "exampleplaintext"
+
+	output, err := DecryptHexStringToString(input)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("Decrypted string: %s", decryptedString)
 
-	if unencryptedString != decryptedString {
-		t.Fatal("Unencrypted string and Decrypted string do not match.")
+	if output != expectedOutput {
+		t.Fatalf("output does not match expected output\noutput: %q\nhex: %x\n\nexpected: %q\nhex: %x\n", output, output, expectedOutput, expectedOutput)
+	}
+}
+
+func TestEncrypt(t *testing.T) {
+	input := []byte{98, 101, 101, 112, 98, 111, 111, 112} // "beepboop"
+	expectedOutput := "beepboop"
+
+	output, err := Encrypt(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	hexout := hex.EncodeToString(output)
+
+	decryptedOutput, err := DecryptHexStringToString(hexout)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if expectedOutput != decryptedOutput {
+		t.Fatalf("output does not match expected output\nout:\t%q\nexp:\t%q\n", decryptedOutput, expectedOutput)
+	}
+}
+
+func TestEncryptStringToHexString(t *testing.T) {
+	input := "000010000"
+	expectedOutput := input
+
+	encryptedOutput, err := EncryptStringToHexString(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decryptedOutput, err := DecryptHexStringToString(encryptedOutput)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if decryptedOutput != expectedOutput {
+		t.Fatalf("output does not match expected output\nout:\t%q\nexp:\t%q\n", decryptedOutput, expectedOutput)
 	}
 }
