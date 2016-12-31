6c43ff750a23bc4b50763e0c54caaa912fb06ab5
puddingfactory <puddingfactory@users.noreply.github.com>
2016-12-30 23:08:52 -0600

    Add test for RememberEntry

--- localstorage/localstorage_test.go
+++ localstorage/localstorage_test.go
@@ -1,6 +1,7 @@
 package localstorage
 
 import (
+	"os"
 	"testing"
 
 	"github.com/puddingfactory/filecabinet/clob"
@@ -11,7 +12,13 @@ const (
 )
 
 var (
-	cache = Cache{Cabinet: testCab}
+	cache     = Cache{Cabinet: testCab}
+	testEntry = clob.Entry{
+		Key:       "5678",
+		ParentKey: "1234",
+		Name:      "test.txt",
+		Type:      '-',
+	}
 )
 
 func TestNew(t *testing.T) {
@@ -21,8 +28,32 @@ func TestNew(t *testing.T) {
 	}
 }
 
+func TestRememberEntry(t *testing.T) {
+	filename := "localstorage.go"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := testEntry // copy
+	e.Name = filename
+	e.Size = fileInfo.Size()
+	e.LastModified = fileInfo.ModTime()
+	e.Body = file
+
+	if err := cache.RememberEntry(e); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestForgetEntry(t *testing.T) {
-	if err := cache.ForgetEntry(clob.Entry{Key: "1234"}); err != nil {
+	if err := cache.ForgetEntry(clob.Entry{Key: testEntry.Key}); err != nil {
 		t.Fatal(err)
 	}
 }
