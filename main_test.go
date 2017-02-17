6d5d3dd1e813112a1fd066a9c68d53534b6623be
puddingfactory <puddingfactory@users.noreply.github.com>
2017-02-16 20:02:55 -0600

    Add text extraction
    Still working through getting a decent threshold, whether that
    threshold has anything to do with font size, etc.

---
+++ main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"rsc.io/pdf"
+)
+
+const (
+	filename          = "testing/test.pdf"
+	threshold float64 = .8
+)
+
+func TestRun(t *testing.T) {
+	r, err := pdf.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 1; i <= r.NumPage(); i++ {
+		var prev pdf.Text
+		for _, text := range r.Page(i).Content().Text {
+
+			var spacing float64
+			var newline bool
+			if prev.Y == text.Y {
+				spacing = text.X - (prev.X + prev.W)
+			} else {
+				newline = true
+			}
+
+			// TODO:detect and handle hyphenation.
+			// If newline, does prev.S equal "-"?
+			// If so, remove hyphen (?) and consider cur to be part of the ongoing word
+
+			// TODO: base threshold on font size?
+			if newline {
+				fmt.Printf("\n\t[%+v|%+v]:%f newline\n", prev, text, spacing)
+			}
+
+			if spacing > threshold {
+				fmt.Printf("\n\t[%+v|%+v]:%f\n", prev, text, spacing)
+			}
+
+			fmt.Print(text.S)
+			// fmt.Printf("%t\t%f\t%s\t%+v\n", newline, spacing, text.S, text)
+			prev = text
+		}
+	}
+	fmt.Println()
+}
