c61eadc46524139ca52a4172823a1e43a2e53756
puddingfactory <puddingfactory@users.noreply.github.com>
2017-03-10 08:18:52 -0600

    Update to detect format changes

--- main_test.go
+++ main_test.go
@@ -20,33 +20,55 @@ func TestRun(t *testing.T) {
 
 	for i := 1; i <= r.NumPage(); i++ {
 		var prev pdf.Text
-		for _, text := range r.Page(i).Content().Text {
+		for _, cur := range r.Page(i).Content().Text {
 
-			var spacing float64
-			var newline bool
-			if prev.Y == text.Y {
-				spacing = text.X - (prev.X + prev.W)
-			} else {
-				newline = true
+			switch true {
+			case formatChanged(prev, cur):
+			case lineChanged(prev, cur):
+			case spacingChanged(prev, cur):
+				fmt.Print("\n")
 			}
 
-			// TODO:detect and handle hyphenation.
-			// If newline, does prev.S equal "-"?
-			// If so, remove hyphen (?) and consider cur to be part of the ongoing word
+			fmt.Print(cur.S)
+			prev = cur
 
-			// TODO: base threshold on font size?
-			if newline {
-				fmt.Printf("\n\t[%+v|%+v]:%f newline\n", prev, text, spacing)
-			}
+			// var spacing float64
+			// var newline bool
+			// if prev.Y == text.Y {
+			// 	spacing = text.X - (prev.X + prev.W)
+			// } else {
+			// 	newline = true
+			// }
 
-			if spacing > threshold {
-				fmt.Printf("\n\t[%+v|%+v]:%f\n", prev, text, spacing)
-			}
+			// // TODO:detect and handle hyphenation.
+			// // If newline, does prev.S equal "-"?
+			// // If so, remove hyphen (?) and consider cur to be part of the ongoing word
 
-			fmt.Print(text.S)
-			// fmt.Printf("%t\t%f\t%s\t%+v\n", newline, spacing, text.S, text)
-			prev = text
+			// // TODO: base threshold on font size?
+			// if newline {
+			// 	fmt.Printf("\n\t[%+v|%+v]:%f newline\n", prev, text, spacing)
+			// }
+
+			// if spacing > threshold {
+			// 	fmt.Printf("\n\t[%+v|%+v]:%f\n", prev, text, spacing)
+			// }
+
+			// fmt.Print(text.S)
+			// // fmt.Printf("%t\t%f\t%s\t%+v\n", newline, spacing, text.S, text)
 		}
 	}
 	fmt.Println()
+}
+
+func formatChanged(prev, cur pdf.Text) bool {
+	return prev.Font != cur.Font || prev.FontSize != cur.FontSize
+}
+
+func lineChanged(prev, cur pdf.Text) bool {
+	return prev.Y != cur.Y
+}
+
+func spacingChanged(prev, cur pdf.Text) bool {
+	// todo
+	return false
 }
