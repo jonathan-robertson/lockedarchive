24a995acc90e9749211f6c78a724bdbb1b5866bf
puddingfactory <puddingfactory@users.noreply.github.com>
2017-07-18 19:17:28 -0500

    Add most of AS3 client

--- cloud/as3.go
+++ cloud/as3.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/puddingfactory/lockedarchive/cache"
 )
 
 // AS3 is used to access AWS S3 services in a way that satisfies the Client interface
@@ -33,33 +35,100 @@ func (client AS3) CreateArchive() error {
 
 // RemoveArchive removes the LockedArchive Bucket
 // TODO: How should this work? Seems pretty unsafe
-func (client *AS3) RemoveArchive() error {
-	return nil
+func (client AS3) RemoveArchive() error {
+	input := &s3.DeleteBucketInput{
+		Bucket: aws.String(client.Bucket),
+	}
+	_, err := client.svc().DeleteBucket(input)
+	// TODO: current design here will error if there are any objcts within bucket
+	// Perhaps we want this behavior for now, exposing another func for delete all obj
+	// or maybe not. bool could be passed into RemoveArchive to confirm if we want to
+	// delete containing objects
+	return evalErr(err)
 }
 
 // List collects all list data for the given bucket
-func (client *AS3) List() ([]Entry, error) {
-	return nil, nil
+func (client AS3) List() (entries []Entry, err error) {
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(client.Bucket),
+	}
+	return entries, evalErr(client.svc().ListObjectsV2Pages(input, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+		for _, obj := range page.Contents {
+			entries = append(entries, Entry{
+				Key:          aws.StringValue(obj.Key),
+				Size:         aws.Int64Value(obj.Size),
+				LastModified: aws.TimeValue(obj.LastModified),
+			})
+		}
+		return aws.BoolValue(page.IsTruncated)
+	}))
+}
+
+// Upload sends an Entry to S3, along with its body and properties
+func (client AS3) Upload(entry Entry) error {
+	input := &s3.PutObjectInput{
+		Bucket: aws.String(client.Bucket),
+		Key:    aws.String(entry.Key),
+		// Body:   aws.ReadSeekCloser(file),
+		// Tagging: aws.String("key1=value1&key2=value2"), // TODO: add this in later
+	}
+	if !entry.IsDir {
+		file, err := cache.Get(entry.Key)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		input.Body = aws.ReadSeekCloser(file)
+	}
+
+	_, err := client.svc().PutObject(input)
+	err = evalErr(err)
+
+	// REVIEW: result returns ETag
+	// NOTE: this may indicate that the system did not perform a hash on the contents (!)
+	// fmt.Printf("%+v\n", result)
+
+	return err
+}
+
+// Download fetches entry's data from S3 and Puts it in cache
+func (client AS3) Download(entry Entry) (err error) {
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(client.Bucket),
+		Key:    aws.String(entry.Key),
+	}
+
+	result, err := client.svc().GetObject(input)
+	if err != nil {
+		return evalErr(err)
+	}
+
+	fmt.Println(result.String()) // TODO: remove
+
+	if aws.Int64Value(result.ContentLength) > 0 {
+		err = cache.Put(entry.Key, result.Body)
+	}
+
+	// TODO: Confirm checksum matches ETAG
+
+	return
 }
 
 //
-func (client *AS3) Upload(Entry) error {
+func (client AS3) Update(Entry) error {
 	return nil
 }
 
-//
-func (client *AS3) Download(Entry) error {
-	return nil
-}
+// Delete removes an Entry from S3
+func (client AS3) Delete(entry Entry) error {
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(client.Bucket),
+		Key:    aws.String(entry.Key),
+	}
 
-//
-func (client *AS3) Update(Entry) error {
-	return nil
-}
-
-//
-func (client *AS3) Delete(Entry) error {
-	return nil
+	_, err := client.svc().DeleteObject(input)
+	err = evalErr(err)
+	return err
 }
 
 func (client AS3) svc() *s3.S3 {
@@ -79,6 +148,8 @@ func evalErr(err error) error {
 			return fmt.Errorf("%s: %s", s3.ErrCodeBucketAlreadyExists, aerr.Error())
 		case s3.ErrCodeBucketAlreadyOwnedByYou:
 			return fmt.Errorf("%s: %s", s3.ErrCodeBucketAlreadyOwnedByYou, aerr.Error())
+		case s3.ErrCodeNoSuchKey:
+			return fmt.Errorf("%s: %s", s3.ErrCodeNoSuchKey, aerr.Error())
 		default:
 			return aerr
 		}
--- cloud/as3_test.go
+++ cloud/as3_test.go
@@ -3,9 +3,54 @@ package cloud
 import "testing"
 
 func TestAS3(t *testing.T) {
-	err := AS3Client("lockedarchive-test", "us-east-1").CreateArchive()
-	if err != nil {
+	var (
+		client = setupAS3(t)
+		entry  = Entry{
+			Key:   "123",
+			IsDir: true,
+		}
+	)
+
+	t.Run("Upload", func(t *testing.T) {
+		if err := client.Upload(entry); err != nil {
+			t.Error(err)
+		}
+	})
+	t.Run("Download", func(t *testing.T) {
+		if err := client.Download(entry); err != nil {
+			t.Error(err)
+		}
+	})
+	t.Run("List", func(t *testing.T) {
+		entries, err := client.List()
+		if err != nil {
+			t.Error(err)
+		}
+		for _, entry := range entries {
+			t.Logf("list: %+v", entry)
+		}
+	})
+	t.Run("Delete", func(t *testing.T) {
+		if err := client.Delete(entry); err != nil {
+			t.Error(err)
+		}
+	})
+
+	teardown(t, client)
+}
+
+func setupAS3(t *testing.T) (client Client) {
+	client = AS3Client("lockedarchive-test", "us-east-1")
+	if err := client.CreateArchive(); err != nil {
 		t.Fatal(err)
 	}
 	t.Log("success: created archive with AS3")
+	return
+}
+
+func teardown(t *testing.T, client Client) {
+	if err := client.RemoveArchive(); err != nil {
+		t.Fatal(err)
+	}
+	t.Log("success: removed archive with AS3")
 }
