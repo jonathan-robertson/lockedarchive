5472f1ea02495ab3dbfd816b2ac39ec10498534b
Jonathan Robertson <5396045+jonathan-robertson@users.noreply.github.com>
2017-10-19 06:39:06 -0500

    Add encryption/decryption with streaming/chunking

--- cache/cache.go
+++ cache/cache.go
@@ -2,142 +2,291 @@
 package cache
 
 import (
-	"bytes"
-	"compress/gzip"
+	"context"
+	"crypto/rand"
+	"errors"
 	"io"
-	"io/ioutil"
-	"os"
 
-	"github.com/gtank/cryptopasta"
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
-// TODO: init should reach out for the user's configuration to get key
-var key *[32]byte // temporary key
+const (
+	// KeySize represents the size of the key in bytes
+	KeySize = 32 // 256-bit
 
-// Write compresses and encrypts file, writing it to the cache; closes file
-func Write(source, destination *os.File) (err error) {
+	// NonceSize represents the size of the nonce in bytes
+	NonceSize = 24 // 192-bit
 
-	// Read all contents of file
-	uncompressed, err := ioutil.ReadAll(source)
-	if err != nil {
-		return
-	}
+	// ChunkSize represents the size of each encrypted chunk in bytes
+	ChunkSize = 16384 // 16kb
+)
 
-	// Compress
-	compressedtext, err := compress(uncompressed)
-	if err != nil {
-		return
-	}
-	if err = source.Close(); err != nil {
-		return
-	}
+var (
+	// ErrEncrypt is an error that occurred during encryption
+	ErrEncrypt = errors.New("secret: encryption failed")
 
-	// Encrypt
-	ciphertext, err := cryptopasta.Encrypt(compressedtext, key)
-	if err != nil {
-		return
-	}
+	// ErrDecrypt is an error that occurred during decryption
+	ErrDecrypt = errors.New("secret: decryption failed")
+)
 
-	// Write
-	if _, err = destination.Write(ciphertext); err != nil {
-		return
+// REVIEW: https://leanpub.com/gocrypto/read#leanpub-auto-nacl
+
+// GenerateKey creates a new random secret key and panics if the source
+// of randomness fails
+// TODO: harvest more entropy?
+func GenerateKey() *[KeySize]byte {
+	key := new([KeySize]byte)
+	if _, err := io.ReadFull(rand.Reader, key[:]); err != nil {
+		panic(err)
 	}
-	return destination.Close()
+	return key
 }
 
-// Read decrypts and decompresses file, returning plaintext
-func Read(name string) (plaintext []byte, err error) {
-
-	// Get file if exists
-	file, err := os.Open(name)
-	if err != nil {
-		return
+// GenerateNonce creates a new random nonce and panics if the source of
+// randomness fails
+func GenerateNonce() *[NonceSize]byte {
+	nonce := new([NonceSize]byte)
+	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
+		panic(err)
 	}
-
-	// Read file contents
-	ciphertext, err := ioutil.ReadAll(file)
-	if err != nil {
-		return
-	}
-
-	// Close file
-	if err = file.Close(); err != nil {
-		return
-	}
-
-	// Decrypt
-	compressed, err := cryptopasta.Decrypt(ciphertext, key)
-	if err != nil {
-		return
-	}
-
-	// Decompress
-	plaintext, err = decompress(compressed)
-	if err != nil {
-		return
-	}
-
-	return
+	return nonce
 }
 
-// Put adds a file to the cache without any modifications
-// This is best used with data received from cloud storage
-// TODO: update to use cache path
-func Put(name string, rc io.ReadCloser) (err error) {
-	file, err := os.Create(name)
-	if err != nil {
-		return
-	}
+// Encrypt generates a random nonce and encrypts the input using
+// NaCl's secretbox package. The nonce is prepended to the ciphertext.
+// A sealed message will the same size as the original message plus
+// secretbox.Overhead bytes long.
+// REVIEW: Keep in mind that random nonces are not always the right choice.
+// We’ll talk more about this in a chapter on key exchanges, where we’ll talk
+// about how we actually get and share the keys that we’re using.
+func Encrypt(key *[KeySize]byte, message []byte) ([]byte, error) {
+	nonce := GenerateNonce()
 
-	_, err = io.Copy(file, rc)
-	file.Close()
-	return
+	out := make([]byte, len(nonce))
+	copy(out, nonce[:])
+	out = secretbox.Seal(out, message, nonce, key)
+	return out, nil
 }
 
-// Get returns readCloser to a cached file; caller responsible for closing
-// This is best used for providing cloud storage the bytes to transmit
-// TODO: update to use cache path
-func Get(name string) (*os.File, error) {
-	return os.Open(name)
+// Decrypt extracts the nonce from the ciphertext, and attempts to
+// decrypt with NaCl's secretbox.
+func Decrypt(key *[KeySize]byte, message []byte) ([]byte, error) {
+	if len(message) < (NonceSize + secretbox.Overhead) {
+		return nil, ErrDecrypt
+	}
+
+	var nonce [NonceSize]byte
+	copy(nonce[:], message[:NonceSize])
+	out, ok := secretbox.Open(nil, message[NonceSize:], &nonce, key)
+	if !ok {
+		return nil, ErrDecrypt
+	}
+
+	return out, nil
 }
 
-// compress responsible for compressing contents of a file
-// REVIEW: update one day to support streaming
-func compress(uncompressed []byte) (compressed []byte, err error) {
+// REVIEW: https://blog.cloudflare.com/recycling-memory-buffers-in-go/
+// manage memory carefully
+// TODO: https://stackoverflow.com/questions/16971741/how-do-you-clear-a-slice-in-go
+// I love you, internet
 
-	// Compress file contents
-	var buf bytes.Buffer
-	zw := gzip.NewWriter(&buf)
-	if _, err = zw.Write(uncompressed); err != nil {
-		return
+// EncryptStream encrypts a stream of data in chunks
+// TODO: explore async options - probably use io.ReadSeeker
+// TODO: update to generate initial nonce, then increment for subsequent chunks
+func EncryptStream(ctx context.Context, key *[KeySize]byte, r io.Reader, w io.Writer) error {
+	chunk := make([]byte, ChunkSize)
+	// nonce := GenerateNonce()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		default:
+			len, err := r.Read(chunk)
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				}
+				return err
+			}
+
+			encryptedChunk, err := Encrypt(key, chunk[:len])
+			if err != nil {
+				return err
+			}
+
+			len, err = w.Write(encryptedChunk)
+			if err != nil {
+				return err
+			}
+		}
 	}
-
-	// Close compression writer
-	if err = zw.Close(); err != nil {
-		return
-	}
-
-	compressed = buf.Bytes()
-	return
 }
 
-// decompress responsible for decompressing contents
-// REVIEW: update one day to support streaming
-func decompress(compressed []byte) (decompressed []byte, err error) {
+// DecryptStream decrypts a stream of data in chunks
+// TODO: explore async options - probably use io.ReadSeeker
+// TODO: update to generate initial nonce, then increment for subsequent chunks
+func DecryptStream(ctx context.Context, key *[KeySize]byte, r io.Reader, w io.Writer) error {
+	chunk := make([]byte, ChunkSize)
 
-	// Create new decompression reader
-	zr, err := gzip.NewReader(bytes.NewReader(compressed))
-	if err != nil {
-		return
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		default:
+			len, err := r.Read(chunk)
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				}
+				return err
+			}
+
+			decryptedChunk, err := Decrypt(key, chunk[:len])
+			if err != nil {
+				return err
+			}
+
+			len, err = w.Write(decryptedChunk)
+			if err != nil {
+				return err
+			}
+		}
 	}
-
-	// Read into bytes
-	decompressed, err = ioutil.ReadAll(zr)
-	if err != nil {
-		return
-	}
-
-	// Close decompression reader
-	err = zr.Close()
-	return
 }
+
+func CompressStream()   {} // TODO
+func DecompressStream() {} // TODO
+
+// // TODO: init should reach out for the user's configuration to get key
+// var key *[32]byte // temporary key
+
+// // Write compresses and encrypts file, writing it to the cache; closes file
+// func Write(source, destination *os.File) (err error) {
+
+// 	// Read all contents of file
+// 	uncompressed, err := ioutil.ReadAll(source)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Compress
+// 	compressedtext, err := compress(uncompressed)
+// 	if err != nil {
+// 		return
+// 	}
+// 	if err = source.Close(); err != nil {
+// 		return
+// 	}
+
+// 	// Encrypt
+// 	ciphertext, err := cryptopasta.Encrypt(compressedtext, key)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Write
+// 	if _, err = destination.Write(ciphertext); err != nil {
+// 		return
+// 	}
+// 	return destination.Close()
+// }
+
+// // Read decrypts and decompresses file, returning plaintext
+// func Read(name string) (plaintext []byte, err error) {
+
+// 	// Get file if exists
+// 	file, err := os.Open(name)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Read file contents
+// 	ciphertext, err := ioutil.ReadAll(file)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Close file
+// 	if err = file.Close(); err != nil {
+// 		return
+// 	}
+
+// 	// Decrypt
+// 	compressed, err := cryptopasta.Decrypt(ciphertext, key)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Decompress
+// 	plaintext, err = decompress(compressed)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	return
+// }
+
+// // Put adds a file to the cache without any modifications
+// // This is best used with data received from cloud storage
+// // TODO: update to use cache path
+// func Put(name string, rc io.ReadCloser) (err error) {
+// 	file, err := os.Create(name)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	_, err = io.Copy(file, rc)
+// 	file.Close()
+// 	return
+// }
+
+// // Get returns readCloser to a cached file; caller responsible for closing
+// // This is best used for providing cloud storage the bytes to transmit
+// // TODO: update to use cache path
+// func Get(name string) (*os.File, error) {
+// 	return os.Open(name)
+// }
+
+// // compress responsible for compressing contents of a file
+// // REVIEW: update one day to support streaming
+// func compress(uncompressed []byte) (compressed []byte, err error) {
+
+// 	// Compress file contents
+// 	var buf bytes.Buffer
+// 	zw := gzip.NewWriter(&buf)
+// 	if _, err = zw.Write(uncompressed); err != nil {
+// 		return
+// 	}
+
+// 	// Close compression writer
+// 	if err = zw.Close(); err != nil {
+// 		return
+// 	}
+
+// 	compressed = buf.Bytes()
+// 	return
+// }
+
+// // decompress responsible for decompressing contents
+// // REVIEW: update one day to support streaming
+// func decompress(compressed []byte) (decompressed []byte, err error) {
+
+// 	// Create new decompression reader
+// 	zr, err := gzip.NewReader(bytes.NewReader(compressed))
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Read into bytes
+// 	decompressed, err = ioutil.ReadAll(zr)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	// Close decompression reader
+// 	err = zr.Close()
+// 	return
+// }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,10 +1,12 @@
 package cache
 
 import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
 	"os"
 	"testing"
-
-	"github.com/gtank/cryptopasta"
 )
 
 const (
@@ -12,41 +14,148 @@ const (
 	destFilename   = "dest.txt"
 )
 
-func setup(t *testing.T) {
-	key = cryptopasta.NewEncryptionKey()
+// func setup(t *testing.T) {
+// 	key = GenerateKey()
+// }
+// func teardown(t *testing.T) {
+// 	if err := os.Remove(destFilename); err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
+
+// func Test(t *testing.T) {
+// 	setup(t)
+// 	defer teardown(t)
+// 	t.Run("Write", func(t *testing.T) { WriteIt(t) })
+// 	t.Run("Read", func(t *testing.T) { ReadIt(t) })
+// }
+
+// func WriteIt(t *testing.T) {
+// 	source, err := os.Open(sourceFilename)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	dest, err := os.OpenFile(destFilename, os.O_CREATE|os.O_WRONLY, 0666)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	if err := Write(source, dest); err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
+
+// func ReadIt(t *testing.T) {
+// 	plaintext, err := Read(destFilename)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	t.Logf("%s", plaintext)
+// }
+
+// func WriteSecret(t *testing.T) {
+// 	source, err := os.Open(sourceFilename)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	dest, err := os.OpenFile(destFilename, os.O_CREATE|os.O_WRONLY, 0666)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	var (
+// 		message []byte
+// 		out     []byte
+// 	)
+
+// 	nonce := GenerateNonce()
+// 	ciphertext := secretbox.Seal(nonce[:], plaintext, nonce, key)
+
+// }
+
+// TestBasicStream is only for testing the basic streaming functionality of golang
+func TestBasicStream(t *testing.T) {
+	r := bytes.NewReader([]byte("Tastey fishies!!"))
+
+	chunk := make([]byte, 3)
+
+	for {
+		len, err := r.Read(chunk)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			t.Fatal(err)
+		}
+		t.Logf("%d bytes read for %s", len, string(chunk[:len]))
+	}
 }
+
+func TestStream(t *testing.T) {
+	var (
+		err            error
+		ctx, cancelCtx = context.WithCancel(context.Background())
+		pr, pw         = io.Pipe() // used to route output from encrypt into input for decrypt
+		key, src, dst  = setup(t)  // build setup objects
+	)
+
+	go func() {
+		if err = DecryptStream(ctx, key, pr, dst); err != nil {
+			cancelCtx()
+		}
+	}()
+
+	if err = EncryptStream(ctx, key, src, pw); err != nil {
+		cancelCtx()
+	}
+
+	// close files before errors are acted on
+	src.Close()
+	dst.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	confirmDstMatchesSrc(t)
+
+	teardown(t)
+}
+
+// setup returns some starting objects; USER RESPONSIBLE FOR CLOSING src and dst
+func setup(t *testing.T) (*[KeySize]byte, *os.File, *os.File) {
+	key := GenerateKey()
+
+	src, err := os.Open(sourceFilename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dst, err := os.OpenFile(destFilename, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return key, src, dst
+}
+
 func teardown(t *testing.T) {
 	if err := os.Remove(destFilename); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func Test(t *testing.T) {
-	setup(t)
-	defer teardown(t)
-	t.Run("Write", func(t *testing.T) { WriteIt(t) })
-	t.Run("Read", func(t *testing.T) { ReadIt(t) })
-}
-
-func WriteIt(t *testing.T) {
-	source, err := os.Open(sourceFilename)
-	if err != nil {
-		t.Fatal(err)
-	}
-	dest, err := os.OpenFile(destFilename, os.O_CREATE|os.O_WRONLY, 0666)
+func confirmDstMatchesSrc(t *testing.T) {
+	srcData, err := ioutil.ReadFile(sourceFilename)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := Write(source, dest); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func ReadIt(t *testing.T) {
-	plaintext, err := Read(destFilename)
+	dstData, err := ioutil.ReadFile(destFilename)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("%s", plaintext)
+
+	if !bytes.Equal(srcData, dstData) {
+		t.Fatal("src and dst are not equal, but they should be")
+	}
 }
