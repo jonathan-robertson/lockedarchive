b1aaab690ec9d5dee60af68f94cc03995ab5d13c
Jonathan Robertson <5396045+jonathan-robertson@users.noreply.github.com>
2017-10-25 05:54:48 -0500

    Add test for chunking of slow-drip read

--- stream/chunk_test.go
+++ stream/chunk_test.go
@@ -3,17 +3,52 @@ package stream_test
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"io"
 	"testing"
+	"time"
 
 	"github.com/jonathan-robertson/lockedarchive/stream"
 )
 
-func TestChunk(t *testing.T) {
+func TestFastChunk(t *testing.T) {
 	src := []byte("This is a test set of data and it is very nice")
 	r := bytes.NewReader(src)
+	dst := runChunkTest(t, r)
+	verifyBytesEqual(t, src, dst)
+}
 
+func TestSlowChunk(t *testing.T) {
+	src := []byte("This is a test set of data and it is very nice")
+	r := bytes.NewReader(src)
+	pr, pw := io.Pipe()
+	go func() {
+		smallBlock := make([]byte, 2)
+		for ; ; time.Sleep(200 * time.Millisecond) { // 200ms to simulate high latency connection
+			n, err := r.Read(smallBlock)
+			if err != nil && err != io.EOF {
+				t.Fatal(err)
+			}
+
+			if n > 0 {
+				if _, writeErr := pw.Write(smallBlock[:n]); writeErr != nil {
+					t.Fatal(writeErr)
+				}
+			}
+
+			if err == io.EOF {
+				break
+			}
+		}
+		if err := pw.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	dst := runChunkTest(t, pr)
+	verifyBytesEqual(t, src, dst)
+}
+
+func runChunkTest(t *testing.T, r io.Reader) []byte {
 	var dst []byte
 
 	chunk := make([]byte, 5)
@@ -36,8 +71,10 @@ func TestChunk(t *testing.T) {
 		}
 	}
 
-	fmt.Printf("dst: %s", dst)
+	return dst
+}
 
+func verifyBytesEqual(t *testing.T, src, dst []byte) {
 	if len(src) != len(dst) {
 		t.Fatalf("length of data pre and post chunked does not match - before: %d after: %d", len(src), len(dst))
 	}
